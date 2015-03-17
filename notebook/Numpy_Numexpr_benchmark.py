d706e108fdae663e5953d9010ec8219f337b5a52
cayetanobv <cayetanobv@gmail.com>
2015-03-17 01:38:49 +0100

    included notebook

---
+++ notebook/Numpy_Numexpr_benchmark.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+%%timeit -n1
+%run /home/cayetano/Dropbox/documentos/python/GIS/Numpy_Numexpr_benchmark/test_numexpr.py
+
+# <codecell>
+
+%%timeit -n1
+%run /home/cayetano/Dropbox/documentos/python/GIS/Numpy_Numexpr_benchmark/test_nonumexpr.py
+
+# <codecell>
+
+%prun testNumexpr(5000)
+
+# <codecell>
+
+%load_ext memory_profiler
+
+# <codecell>
+
+%load_ext line_profiler
+
+# <codecell>
+
+%lprun -f testNumexpr testNumexpr(5000)
+
+# <codecell>
+
+%mprun -f testNumexpr testNumexpr(5000)
+
+# <codecell>
+
+%prun testNoNumexpr(5000)
+
+# <codecell>
+
+%lprun -f testNumexpr testNoNumexpr(5000)
+
+# <codecell>
+
+%mprun -f testNumexpr testNoNumexpr(5000)
+
+# <codecell>
+
+
